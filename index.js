ce6d2b7e89f2c18b1b71c197e44c0effc5587212
MikeDudolevitch <mikedudolevitch@gmail.com>
2021-06-19 00:14:29 -0400

    commit tree 65665874ffbfcbd136e46ee9d90edf87a8983b12

--- index.js
+++ index.js
@@ -10,4 +10,15 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(str) {
+  const hour = parseInt(str, 10)
+  if (hour < 12) return "Good Morning"
+  if (hour > 17) return "Good Evening"
+  return "Good Afternoon"
+}
+
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message
+
+}
 /* Write your implementation of displayMessage() */
